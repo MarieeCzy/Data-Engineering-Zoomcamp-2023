13f60cb5e378d54a24b2a19bcb98199f2bbd907c
__eMCe__ <93644245+MarieeCzy@users.noreply.github.com>
2023-01-30 19:45:16 +0100

    Add homework folder

---
+++ week1_homework/ingest_data_hw.py
@@ -0,0 +1,79 @@
+import pandas as pd
+from sqlalchemy import create_engine
+from time import time
+import  argparse
+import os
+
+def main(params):
+    
+    user = params.user
+    password = params.password
+    host = params.host
+    port = params.port
+    db = params.db
+    table_name = params.table_name
+    url = params.url
+    csv_name = 'output.csv.gz'
+    
+    if url.endswith('.csv.gz'):
+        csv_name = 'output.csv.gz'
+        #dwonload the csv
+        os.system(f"python3 -m wget {url} -o {csv_name}")
+        
+    else:
+        csv_name = f'{url}'
+    
+    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
+
+    df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
+    df = next(df_iter)
+
+    try:
+        df.lpep_pickup_datetime = pd.to_datetime(df.lpep_pickup_datetime)
+        df.lpep_dropoff_datetime = pd.to_datetime(df.lpep_dropoff_datetime)
+    except AttributeError:
+        pass
+ 
+ 
+    df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
+
+    #load first data to table
+    df.to_sql(name=table_name, con=engine, if_exists='append')
+
+    #load rest of chunks
+    while True:
+        try:
+            start_time = time()
+            df = next(df_iter)
+            
+            try:
+                df.lpep_pickup_datetime = pd.to_datetime(df.lpep_pickup_datetime)
+                df.lpep_dropoff_datetime = pd.to_datetime(df.lpep_dropoff_datetime)
+            except AttributeError:
+                pass
+            
+            df.to_sql(name=table_name, con=engine, if_exists='append')
+            end_time = time()
+        
+            print('Another chunk loaded in: %3f seconds.' %(end_time - start_time))
+    
+        except StopIteration:
+            print("Finished ingesting data into the postgres database")
+            break
+        
+if __name__ == '__main__':  
+    parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres')
+
+    parser.add_argument('--user', help='username for postgres')
+    parser.add_argument('--password', help='password for postgres')
+    parser.add_argument('--host', help='host for postgres')
+    parser.add_argument('--port', help='port for postgres')
+    parser.add_argument('--db', help='database name for postgres')
+    parser.add_argument('--table_name', help='name of the table where we will wirte the results to')
+    parser.add_argument('--url', help='url of the csv file')
+
+    args = parser.parse_args()
+    
+    main(args)
+    
+  
