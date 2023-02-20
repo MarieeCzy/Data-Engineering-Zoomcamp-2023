8392c6b0e1cb43a0e3e179c928b46c9e67b3ec5a
__eMCe__ <93644245+MarieeCzy@users.noreply.github.com>
2023-02-20 17:32:08 +0100

    Create ETL pipeline with BigQuery

---
+++ week2_prefect/02_gcp/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "focus-poet-376519"
+}
+
+#resource "google_storage_bucket" "data-lake-bucket-maria"{
+#  name     = "random-bucket-maria-123"
+#  location = "EU"
+#}
+
+resource "google_bigquery_dataset" "default" {
+  dataset_id = "dezoomcamp"
+  location = "EU"
+}
+
+resource "google_bigquery_table" "default"{
+  dataset_id = google_bigquery_dataset.default.dataset_id
+  table_id = "rides"
+}
