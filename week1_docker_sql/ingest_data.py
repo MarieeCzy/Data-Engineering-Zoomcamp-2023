b09e10c8a7a0f4014dd64211b97c969d49e71bfc
__eMCe__ <93644245+MarieeCzy@users.noreply.github.com>
2023-03-08 11:21:36 +0100

    Add docker-compose

--- week1_docker_sql/ingest_data.py
+++ week1_docker_sql/ingest_data.py
@@ -67,26 +67,3 @@ if __name__ == '__main__':
     
     main(args)
     
-    '''
-    python3 ingest_data.py \
-        --user=root \
-        --password=... \
-        --host=localhost \
-        --port=5432 \
-        --db=ny_taxi \
-        --table_name=yellow_taxi_trips \
-        --url="https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2019-01.csv.gz"
-    '''
-    '''
-    docker run -it \
-        --network=pg-network \
-        taxi_ingest:v001 \
-            --user=root \
-            --password=root \
-            --host=pg-database \
-            --port=5432 \
-            --db=ny_taxi \
-            --table_name=yellow_taxi_trips \
-            --url="https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2019-01.csv.gz"
-        
-    '''
